e386162e74638bf94838b2133ea9d7c8666b5501
Alexander Evstratov <alexdrugev@me.com>
2024-04-05 01:59:11 +0300

    Add music.

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -5,6 +5,8 @@ using TMPro;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text _bestScore;
+    [SerializeField] private GameObject _musicOn;
+    [SerializeField] private GameObject _musicOff;
 
     public void StartGame()
     {
@@ -15,5 +17,34 @@ public class MainMenu : MonoBehaviour
     {
         int bestScore = PlayerPrefs.GetInt("bestScore", 0);
         _bestScore.text = $"BEST SCORE: {bestScore}";
+
+        AudioListener.volume = PlayerPrefs.GetFloat("music", 1);
+        if (AudioListener.volume == 1)
+        {
+            MusicOn();
+        }
+        else
+        {
+            MusicOff();
+        }
+    }
+
+    public void MusicOff()
+    {
+        _musicOn.SetActive(false);
+        _musicOff.SetActive(true);
+        AudioListener.volume = 0;
+    }
+
+    public void MusicOn()
+    {
+        _musicOn.SetActive(true);
+        _musicOff.SetActive(false);
+        AudioListener.volume = 1;
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat("music", AudioListener.volume);
     }
 }
