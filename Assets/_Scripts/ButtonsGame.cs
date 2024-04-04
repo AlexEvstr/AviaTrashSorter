1ae5c5b80684ef64554e94af3861e3144045bd19
Alexander Evstratov <alexdrugev@me.com>
2024-04-05 01:26:57 +0300

    Add score and best score.

--- Assets/_Scripts/ButtonsGame.cs
+++ Assets/_Scripts/ButtonsGame.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonsGame : MonoBehaviour
 {
     [SerializeField] private GameObject _pausePanel;
+
+    public void OpenPause()
+    {
+        _pausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ClosePause()
+    {
+        _pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
 }
--- Assets/_Scripts/ScoreCounter.cs
+++ Assets/_Scripts/ScoreCounter.cs
@@ -6,16 +6,27 @@ using TMPro;
 public class ScoreCounter : MonoBehaviour
 {
     [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private TMP_Text _bestPause;
+    [SerializeField] private TMP_Text _bestFinish;
     public static int Score;
+    private int _bestScore; 
 
     private void Start()
     {
+        _bestScore = PlayerPrefs.GetInt("bestScore", 0);
         Score = 0;
     }
 
     private void Update()
     {
-        if (Score < 0) Score = 0; 
+        if (Score < 0) Score = 0;
+        if (Score > _bestScore)
+        {
+            _bestScore = Score;
+            PlayerPrefs.SetInt("bestScore", _bestScore);
+        }
         _scoreText.text = $"Score: {Score}";
+        _bestPause.text = $"BEST SCORE: {_bestScore}";
+        _bestFinish.text = $"BEST SCORE: {_bestScore}";
     }
 }
